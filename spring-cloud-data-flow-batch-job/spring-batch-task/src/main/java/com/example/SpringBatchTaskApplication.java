1a9769f971187cdbb09870741ab4eba8e6d7ca7f
Prateek <prateek.ashtikar512@gmail.com>
2020-05-02 11:43:56 +0530

    Spring Cloud Data Flow and Spring Batch

---
+++ spring-cloud-data-flow-batch-job/spring-batch-task/src/main/java/com/example/SpringBatchTaskApplication.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.task.configuration.EnableTask;
+
+
+@EnableTask
+@SpringBootApplication
+public class SpringBatchTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchTaskApplication.class, args);
+	}
+
+}
